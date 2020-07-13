700902f7792da6b712e154068a47a44f3a13a2a1
afresquet <alvarofresquet@gmail.com>
2020-07-13 20:47:24 +0200

    Updates

---
+++ precache-manifest.9cdc3224f445de48853e73edf4bdb249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c5d69733205410577697ede1f69665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d8b42a0b931af1aa2c",
+    "url": "/static/js/2.7352993c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7352993c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c526e49e7fdb6830b82a",
+    "url": "/static/js/main.7009b2f9.chunk.js"
+  },
+  {
+    "revision": "1663f1e3b59ca2d10f4f",
+    "url": "/static/js/runtime-main.97225a13.js"
+  }
+]);
