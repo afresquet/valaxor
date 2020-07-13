67adebced226f87c5e93efaaf230eb8aa5766b20
afresquet <alvarofresquet@gmail.com>
2020-07-13 21:05:26 +0200

    minor styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import React from "react";
 
 function App() {
 	return (
-		<div>
-			<div>Valaxor</div>
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				height: "100vh",
+			}}
+		>
+			<h1 style={{ fontSize: 8 * 20 }}>Valaxor</h1>
 
-			<div>
-				<a href="http://twitter.valaxor.com">Twitter</a>
+			<div style={{ display: "flex" }}>
+				<a href="http://twitter.valaxor.com" style={{ margin: "0 16px" }}>
+					Twitter
+				</a>
+
+				<a href="http://twitch.valaxor.com" style={{ margin: "0 16px" }}>
+					Twitch
+				</a>
+
+				<a href="http://randomizer.valaxor.com" style={{ margin: "0 16px" }}>
+					Randomizer
+				</a>
 			</div>
 
-			<div>
-				<a href="http://twitch.valaxor.com">Twitch</a>
-			</div>
-
-			<div>
-				<a href="http://randomizer.valaxor.com">Randomizer</a>
-			</div>
+			<p>
+				Website is a WIP, mostly to redirect to the{" "}
+				<a href="http://randomizer.valaxor.com">Randomizer website</a>.
+			</p>
 		</div>
 	);
 }
